5bf15b4262d537563cf2774ca9630100d60ba033
Christophe <christophe.pele@gmail.com>
2013-07-17 08:21:18 +0200

    Import jersey/angularjs skeleton

---
+++ lost-in-transit/src/main/webapp/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('webappApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $http.get('../webresources/myresource').success(function(data) { $scope.awesomeThings.push(data) })
+
+  });
