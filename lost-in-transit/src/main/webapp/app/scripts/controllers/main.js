f5b10edd681539cdbe2e8bf95850ff9148ed1a5a
Christophe <christophe.pele@gmail.com>
2013-07-23 21:17:52 +0200

    Keep previous scores in main view

--- lost-in-transit/src/main/webapp/app/scripts/controllers/main.js
+++ lost-in-transit/src/main/webapp/app/scripts/controllers/main.js
@@ -4,9 +4,16 @@ angular.module('webappApp').controller('MainCtrl', function($scope, $http) {
 
 	$scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
 
+	if ($scope.scores == undefined) {
+		$scope.scores = [];
+	}
+
 	$scope.computeScore = function() {
 		$http.get('../webresources/myresource/' + $scope.address).success(function(data) {
-			$scope.score = data;
+			$scope.scores.push({
+				address : $scope.address,
+				score : data
+			});
 		});
 	};
 
